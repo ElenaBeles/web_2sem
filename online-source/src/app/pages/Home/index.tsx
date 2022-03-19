0496de987ac88bd92defbd5188c48195b7ff59c9
ElenaBeles <beles02@bk.ru>
2022-03-19 21:23:24 +0300

    1

--- online-source/src/app/pages/Home/index.tsx
+++ online-source/src/app/pages/Home/index.tsx
@@ -33,63 +33,65 @@ let colorsSlide: {color: string}[] = [
 export const Home = observer(() => {
     return (
         <LayoutContainer>
-            <div className={ styles.slider }>
-                <Swiper
-                    className={ styles.slider__container }
-                    modules={[Navigation]}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    loop = { true }
-                    navigation = { {
-                        nextEl: '.swiper-button-next-u',
-                        prevEl: '.swiper-button-prev-u',
-                    }}>
-                    <SwiperSlide>
-                        <Slide
-                            title={"Сделаем мир чище"}
-                            text={"Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"}
-                            btn_text={"Условия сервиса"}
-                            color = {colorsSlide[0].color}
-                            img={ first_slide }/>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Slide
-                            title={"А вы знали..."}
-                            text={"что среднее время разложения пластмассовых изделий колеблется от 400 до 700 лет,  а полиэтиленовых пакетов — от 100 до 200 лет? "}
-                            btn_text={"Узнать больше"}
-                            color = {colorsSlide[1].color}
-                            img={ second_slide }/>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Slide
-                            title={"Что с масками?"}
-                            text={"Медицинские маски не обязательно должны становиться отходами. Их тоже можно сдать на переработку."}
-                            btn_text={"Пункты сбора масок"}
-                            color = {colorsSlide[2].color}
-                            img={ third_slide }/>
-                    </SwiperSlide>
-                </Swiper>
-                <div className={ styles.slider__navigation }>
-                <div className="swiper-button-prev-u">
-                    <Icon name='prev' size='20'/>
+            <div className={ styles.wrapper }>
+                <div className={ styles.slider }>
+                    <Swiper
+                        className={ styles.slider__container }
+                        modules={[Navigation]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        loop = { true }
+                        navigation = { {
+                            nextEl: '.swiper-button-next-u',
+                            prevEl: '.swiper-button-prev-u',
+                        }}>
+                        <SwiperSlide>
+                            <Slide
+                                title={"Сделаем мир чище"}
+                                text={"Сдай макулатуру или старую одежду и получи скидку на покупку товаров из переработанных материалов"}
+                                btn_text={"Условия сервиса"}
+                                color = {colorsSlide[0].color}
+                                img={ first_slide }/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Slide
+                                title={"А вы знали..."}
+                                text={"что среднее время разложения пластмассовых изделий колеблется от 400 до 700 лет,  а полиэтиленовых пакетов — от 100 до 200 лет? "}
+                                btn_text={"Узнать больше"}
+                                color = {colorsSlide[1].color}
+                                img={ second_slide }/>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Slide
+                                title={"Что с масками?"}
+                                text={"Медицинские маски не обязательно должны становиться отходами. Их тоже можно сдать на переработку."}
+                                btn_text={"Пункты сбора масок"}
+                                color = {colorsSlide[2].color}
+                                img={ third_slide }/>
+                        </SwiperSlide>
+                    </Swiper>
+                    <div className={ styles.slider__navigation }>
+                        <div className="swiper-button-prev-u">
+                            <Icon name='prev' size='20'/>
+                        </div>
+                        <div className="swiper-button-next-u">
+                            <Icon name='next' size='20'/>
+                        </div>
+                    </div>
                 </div>
-                <div className="swiper-button-next-u">
-                    <Icon name='next' size='20'/>
-                </div>
-                </div>
-            </div>
 
-            <div className={ styles.additions }>
-                <Card
-                    img = { map }
-                    title = { "Пункты сбора" }
-                    text = { "Посмотри, где в твоем городе можно сдать вторсырье на переработку" }
-                    link={ "/collection_points"}/>
-                <Card
-                    img = { ecomarket }
-                    title = { "ЭкоМаркет" }
-                    text = { "Используй заработанные экокоины для покупки товаров из переработанных материалов"}
-                    link={ "/ecomarket"}/>
+                <div className={ styles.additions }>
+                    <Card
+                        img = { map }
+                        title = { "Пункты сбора" }
+                        text = { "Посмотри, где в твоем городе можно сдать вторсырье на переработку" }
+                        link={ "/collection_points"}/>
+                    <Card
+                        img = { ecomarket }
+                        title = { "ЭкоМаркет" }
+                        text = { "Используй заработанные экокоины для покупки товаров из переработанных материалов"}
+                        link={ "/ecomarket"}/>
+                </div>
             </div>
         </LayoutContainer>
     )
